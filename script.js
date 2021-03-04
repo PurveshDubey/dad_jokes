4f61ee99ba550186fd9f1ae4ffce5912a3950de1
PurveshDubey <63962915+PurveshDubey@users.noreply.github.com>
2021-03-04 23:12:20 +0530

    Add files via upload
    simple project in html,css and javascript using jokes api

---
+++ script.js
@@ -0,0 +1,33 @@
+let jokeEl = document.getElementById('joke');
+let jokeBtn = document.getElementById('jokebtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+
+//using async/await
+async function generateJoke() {
+    let config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+
+    let res = await fetch('https://icanhazdadjoke.com', config);
+    let data = await res.json();
+    jokeEl.innerHTML = data.joke;
+}
+
+//using .then
+/*function generateJoke() {
+    let config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+fetch('https://icanhazdadjoke.com', config)
+    .then((res) => res.json())
+    .then((data) => {
+        jokeEl.innerHTML = data.joke;
+    });
+}*/
